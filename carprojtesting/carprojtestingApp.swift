4a9794f77f010f1b166da1b8cb967bd410395ae1
Ethan Chen <ethanctrooper@gmail.com>
2022-01-04 18:36:29 -0800

    Initial Commit

---
+++ carprojtesting/carprojtestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  carprojtestingApp.swift
+//  carprojtesting
+//
+//  Created by Ethan Chen on 1/4/22.
+//
+
+import SwiftUI
+
+@main
+struct carprojtestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
